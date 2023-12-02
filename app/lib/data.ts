59ef1ce3ceed931bc6d273d97eae59873011c2ad
mhconradt <mhconradt@protonmail.com>
2023-12-02 12:26:30 -0800

    Finish Chapter 7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,8 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
+
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
